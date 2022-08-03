71ca6bf45fbc856af7741ac57fdd850fd8cd7f24
BorderPaws <a.pawlow2009@ya.ru>
2022-08-03 14:29:23 +0300

    Created a script to rotate and move the enemy towards the player

--- Assets/GG/Scripts/Enemy.cs
+++ Assets/GG/Scripts/Enemy.cs
@@ -4,32 +4,48 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float movementSpeed;
-    public float rotationRate = 360f;
+    public float movementSpeed = 2f;
+    private Rigidbody2D rb2D;
 
-    private Vector3 moveVector;
-    void HandleInput()
+    //Calculates the angle between this object and the player
+    private Vector2 directionToPlayer
     {
-        moveVector.x = Input.GetAxis("Horizontal");
-        moveVector.z = Input.GetAxis("Vertical");
+        get
+        {
+            var horizontal = GameObject.FindGameObjectsWithTag("Player")[0].transform.position.x - transform.position.x;
+            var vertical = GameObject.FindGameObjectsWithTag("Player")[0].transform.position.y - transform.position.y;
+
+            return new Vector2(horizontal, vertical);
+        }
     }
-    float Angle360(Vector3 from, Vector3 to, Vector3 right)
+    float Moves()
     {
-        float angle = Vector3.Angle(from, to);
-        return (Vector3.Angle(right, to) > 90f) ? 360f - angle : angle;
+        if (directionToPlayer.x != 0 || directionToPlayer.y != 0)
+            return 1f;
+        else
+            return 0f;
+    }
+    //Moves this object to the player
+    private void MovementLogic()
+    {
+        transform.Translate(Moves() * transform.up * movementSpeed * Time.fixedDeltaTime, Space.World);
+    }
+    //Turns the face of this object towards the player
+    private void LookToPlayer()
+    {
+        float angle = (Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) - Mathf.PI / 2) * Mathf.Rad2Deg;
+        rb2D.rotation = angle;
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-
-        transform.Rotate(Vector3.up, Angle360(transform.forward, moveVector, transform.right));
-
+        LookToPlayer();
+        MovementLogic();
     }
 }
