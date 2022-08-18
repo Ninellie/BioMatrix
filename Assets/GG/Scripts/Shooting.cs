0708dae8094abbbb80e5199120cf7c42fdf3efd2
Ninellie <a.pawlow2009@ya.ru>
2022-08-18 21:29:12 +0300

    v0.3.1 Added: Improved shooting mechanics - now if you press the shot key, bullets appear every certain interval (rate of fire). In addition, it is impossible to cause a bullet to appear bypassing the rate of fire.

--- Assets/GG/Scripts/Shooting.cs
+++ Assets/GG/Scripts/Shooting.cs
@@ -2,35 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
+using System.Diagnostics;
 
 public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
     public float bulletForce = 200f;
-    public float fireRate = 1f;
+    public float fireRate = 10f;
     public Vector2 mousePosition;
     public Vector2 direction;
 
-    //private bool isPressed;
-    public void OnFire(InputValue input)
+    //Is the fire button pressed
+    public bool isFire;
+
+    public Stopwatch stopwatch = new();
+
+    public float GetTotalSeconds()
     {
-        mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        //direction = Lib2DMethods.DirectionToMe(mousePosition);
-        direction = Lib2DMethods.DirectionToTarget(mousePosition, firePoint.position);
-
-
-        Shoot();
+        TimeSpan ts = stopwatch.Elapsed;
+        return (float)ts.TotalSeconds;
     }
     // Start is called before the first frame update
     void Start()
     {
-        //firePoint = this.transform;
+        isFire = false;
+        stopwatch.Start();
     }
-    void Shoot()
+    //If the reload has passed, then the shot is fired 
+    public void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb2D = bullet.GetComponent<Rigidbody2D>();
-        rb2D.AddForce(direction.normalized * bulletForce, ForceMode2D.Impulse);
+        if (GetTotalSeconds() >= (1f / fireRate))
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            direction = Lib2DMethods.DirectionToTarget(mousePosition, firePoint.position);
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D rb2D = bullet.GetComponent<Rigidbody2D>();
+            rb2D.AddForce(direction.normalized * bulletForce, ForceMode2D.Impulse);
+            stopwatch.Restart();
+        }
+    }
+    public void OnFire()
+    {
+        isFire = true;
+    }
+    public void OnFireOff()
+    {
+        isFire = false;
+    }
+    private void Update()
+    {
+        //Shoot if the fire button is pressed
+        if (isFire)
+        {
+            Shoot();
+        }
     }
 }
