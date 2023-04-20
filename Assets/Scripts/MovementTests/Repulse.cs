44d7236a025d93801d26f0561789c1eebd1d4ea3
Ninellie <a.pawlow2009@ya.ru>
2023-04-21 00:14:15 +0300

    Cant fix repulse bug with rotating enemies

--- Assets/Scripts/MovementTests/Repulse.cs
+++ Assets/Scripts/MovementTests/Repulse.cs
@@ -5,7 +5,7 @@ public class Repulse : MonoBehaviour
 {
     [SerializeField] private float _baseRepulseForce;
     [SerializeField] private float _penetrationDistanceMultiplier;
-    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private string _layerName;
 
     private CircleCollider2D _circleCollider;
 
@@ -13,12 +13,10 @@ public class Repulse : MonoBehaviour
     {
         _circleCollider = GetComponent<CircleCollider2D>();
     }
-
     void OnCollisionStay2D(Collision2D collision)
     {
         Collider2D otherCollider2D = collision.collider;
-        if (otherCollider2D.gameObject.layer != _layerMask) return;
-        //if (otherCollider2D.gameObject.layer != LayerMask.NameToLayer("Enemy")) return;
+        if (otherCollider2D.gameObject.layer != LayerMask.NameToLayer(_layerName)) return;
         var circleColliderOther = otherCollider2D.gameObject.GetComponent<CircleCollider2D>();
 
         Vector2 pushDirection = (transform.position - otherCollider2D.transform.position).normalized;
@@ -34,25 +32,4 @@ public class Repulse : MonoBehaviour
 
         transform.Translate(pushDirection * repulseForce * Time.deltaTime);
     }
-
-    //void OnTriggerStay2D(Collider2D otherCollider2D)
-    //{
-    //    if (otherCollider2D.gameObject.layer == LayerMask.NameToLayer("Enemy"))
-    //    {
-    //        var circleColliderOther = otherCollider2D.gameObject.GetComponent<CircleCollider2D>();
-            
-    //        Vector2 pushDirection = (transform.position - otherCollider2D.transform.position).normalized;
-
-    //        float distance = Vector2.Distance(transform.position, otherCollider2D.transform.position);
-    //        float radiiSum = _circleCollider.radius + circleColliderOther.radius;
-    //        float multipliedRadiiSum = radiiSum * _penetrationDistanceMultiplier;
-    //        float penetrationDepth = multipliedRadiiSum - distance;
-
-    //        penetrationDepth = Mathf.Clamp(penetrationDepth, 0f, Mathf.Infinity);
-
-    //        float repulseForce = _baseRepulseForce * penetrationDepth;
-
-    //        transform.Translate(pushDirection * repulseForce * Time.deltaTime);
-    //    }
-    //}
 }
