7e003660aa4ebb868a7c25efe17966820bb4887b
Ninellie <a.pawlow2009@ya.ru>
2023-04-21 05:13:31 +0300

    Repulse changes

--- Assets/Scripts/MovementTests/Repulse.cs
+++ Assets/Scripts/MovementTests/Repulse.cs
@@ -17,24 +17,6 @@ public class Repulse : MonoBehaviour
             _circleCollider = GetComponent<CircleCollider2D>();
         }
     }
-    //void OnCollisionStay2D(Collision2D collision)
-    //{
-    //    Collider2D otherCollider2D = collision.collider;
-    //    if (otherCollider2D.gameObject.layer != LayerMask.NameToLayer(_layerName)) return;
-
-    //    var circleColliderOther = otherCollider2D.gameObject.GetComponent<CircleCollider2D>();
-    //    Vector2 pushDirection = (transform.position - otherCollider2D.transform.position).normalized;
-    //    float distance = Vector2.Distance(transform.position, otherCollider2D.transform.position);
-    //    float radiiSum = _circleCollider.radius + circleColliderOther.radius;
-    //    float multipliedRadiiSum = radiiSum * _penetrationDistanceMultiplier;
-    //    float penetrationDepth = multipliedRadiiSum - distance;
-
-    //    penetrationDepth = Mathf.Clamp(penetrationDepth, 0f, Mathf.Infinity);
-
-    //    float repulseForce = _baseRepulseForce * penetrationDepth;
-
-    //    transform.Translate(pushDirection * repulseForce * Time.deltaTime);
-    //}
     void OnCollisionStay2D(Collision2D collision)
     {
         Collider2D otherCollider2D = collision.collider;
