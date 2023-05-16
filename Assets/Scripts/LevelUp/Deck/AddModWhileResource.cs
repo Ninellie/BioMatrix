3d0075c8357568f5c90d251ad566bbf6e5c5f9d4
Ninellie <a.pawlow2009@ya.ru>
2023-05-16 17:35:48 +0300

    smolfix

--- Assets/Scripts/LevelUp/Deck/AddModWhileResource.cs
+++ Assets/Scripts/LevelUp/Deck/AddModWhileResource.cs
@@ -2,7 +2,6 @@
 
 public class AddModWhileResource : IEffect
 {
-
     public string Name { get; set; }
     public string Description { get; set; }
     public List<(StatModifier mod, string statName)> Modifiers { get; set; }
