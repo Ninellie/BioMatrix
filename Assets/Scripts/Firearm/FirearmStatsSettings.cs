ddd4a6f39b7601892b7a6c2c3e4f9bdc05afb221
Ninellie <a.pawlow2009@ya.ru>
2023-06-07 01:14:58 +0300

    Slightly improved the logic for adding a firearm projectile size modifier.

--- Assets/Scripts/Firearm/FirearmStatsSettings.cs
+++ Assets/Scripts/Firearm/FirearmStatsSettings.cs
@@ -4,15 +4,15 @@ namespace Assets.Scripts.Firearm
 {
     public class FirearmStatsSettings : MonoBehaviour
     {
-        public float damage;
-        public float shootForce;
-        public float shootsPerSecond;
-        public float maxShootDeflectionAngle;
-        public float magazineCapacity;
-        public float reloadSpeed;
-        public float singleShootProjectile;
-        public float addedProjectileSize;
-        public float addedProjectilePierce;
-        public float addedProjectileKnockback;
+        [SerializeField, Min(0)] public float damage;
+        [SerializeField, Min(0)] public float shootForce;
+        [SerializeField, Min(0)] public float shootsPerSecond;
+        [SerializeField, Min(0)] public float maxShootDeflectionAngle;
+        [SerializeField, Min(1)] public float magazineCapacity;
+        [SerializeField, Min(0.01f)] public float reloadSpeed;
+        [SerializeField, Min(1)] public float singleShootProjectile;
+        [SerializeField] public float projectileSizeMultiplier;
+        [SerializeField, Min(0)] public float projectilePierceCount;
+        [SerializeField] public float addedProjectileKnockback;
     }
 }
