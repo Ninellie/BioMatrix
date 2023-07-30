a504e092998d32859c69a4db017c80008d25dead
Ninellie <a.pawlow2009@ya.ru>
2023-07-30 23:42:15 +0300

    REFACTOR: Stat class cleaning

--- Assets/Scripts/EntityComponents/Stats/Stat.cs
+++ Assets/Scripts/EntityComponents/Stats/Stat.cs
@@ -11,43 +11,13 @@ namespace Assets.Scripts.EntityComponents.Stats
         public event Action ValueChangedEvent;
         private float BaseValue { get; }
         private bool IsModifiable { get; }
-        private readonly float _baseAddedValue = 0;
+        private const float BaseAddedValue = 0;
         private float AddedValue => GetAddedValue();
-        private readonly float _baseMultiplierValue = 100;
+        private const float BaseMultiplierValue = 100;
         private float MultiplierValue => GetMultiplierValue();
         private readonly List<StatModifier> _modifiers = new();
         private const float MultiplierDivisor = 100;
-        //private readonly GameTimeScheduler _gameTimeScheduler;
-        //Without multiplierValue (with multiplierValue = 1)
-        //public Stat(float baseValue, bool isModifiable) : this(baseValue, isModifiable, 1)
-        //{
-        //}
-        //public Stat(float baseValue, List<StatModifier> modifiers) : this(baseValue, 1, modifiers)
-        //{
-        //}
-        //Without addedValue (with addedValue = 0)
-        //public Stat(float baseValue, float baseMultiplierValue, List<StatModifier> modifiers) : this(baseValue, baseMultiplierValue, 0, modifiers)
-        //{
-        //}
-        //public Stat(float baseValue, bool isModifiable, float baseMultiplierValue) : this(baseValue, isModifiable, baseMultiplierValue, 0)
-        //{
-        //}
-        //Base constructors
-        //public Stat(float baseValue, float baseMultiplierValue, float baseAddedValue, List<StatModifier> modifiers) : this(baseValue, true, baseMultiplierValue, baseAddedValue)
-        //{
-        //    _modifiers = modifiers;
-        //}
-        public Stat() : this(0, true)
-        {
-        }
-        public Stat(float baseValue) : this(baseValue, true)
-        {
-        }
-        public Stat(float baseValue, bool isModifiable)
-        {
-            BaseValue = baseValue;
-            IsModifiable = isModifiable;
-        }
+
         public void AddModifier(StatModifier modifier)
         {
             Debug.Log($"Try to add modifier {modifier.Value} {modifier.Type}");
@@ -56,11 +26,6 @@ namespace Assets.Scripts.EntityComponents.Stats
             TryInvokeOnValueChangedEvent(oldValue);
             Debug.Log($"Added mod {modifier.Type} : {modifier.Value}. Is mod temporary?: {modifier.IsTemporary}.");
             Debug.Log($"New stat value: {Value}. Old value: {oldValue}.");
-            //if (modifier.IsTemporary)
-            //{
-            //    Debug.Log($"Scheduled to remove modifier {modifier.Type} : {modifier.Value}. Will be removed after {modifier.Duration} secs.");
-            //    _gameTimeScheduler.Schedule(() => RemoveModifier(modifier), modifier.Duration);
-            //}
         }
 
         public bool RemoveModifier(StatModifier modifier)
@@ -80,6 +45,7 @@ namespace Assets.Scripts.EntityComponents.Stats
         {
             return _modifiers.Count == 0;
         }
+
         public void ClearModifiersList()
         {
             if (IsModifierListEmpty()) return;
@@ -87,28 +53,46 @@ namespace Assets.Scripts.EntityComponents.Stats
             _modifiers.Clear();
             TryInvokeOnValueChangedEvent(oldValue);
         }
+
         private float GetAddedValue()
         {
-            return _baseAddedValue + _modifiers
+            return BaseAddedValue + _modifiers
                 .Where(modifier => modifier.Type == OperationType.Addition)
                 .Sum(modifier => modifier.Value);
         }
+
         private float GetMultiplierValue()
         {
-            return _baseMultiplierValue + _modifiers
+            return BaseMultiplierValue + _modifiers
                 .Where(modifier => modifier.Type == OperationType.Multiplication)
                 .Sum(modifier => modifier.Value);
         }
+
         private float GetActualValue()
         {
             return IsModifiable == false
-                ? (BaseValue + _baseAddedValue) * (_baseMultiplierValue / MultiplierDivisor)
+                ? (BaseValue + BaseAddedValue) * (BaseMultiplierValue / MultiplierDivisor)
                 : (BaseValue + AddedValue) * (MultiplierValue / MultiplierDivisor);
         }
+
         private void TryInvokeOnValueChangedEvent(float oldValue)
         {
             if (Value.Equals(oldValue)) return;
             ValueChangedEvent?.Invoke();
+        }
+
+        public Stat() : this(0, true)
+        {
+        }
+
+        public Stat(float baseValue) : this(baseValue, true)
+        {
+        }
+
+        public Stat(float baseValue, bool isModifiable)
+        {
+            BaseValue = baseValue;
+            IsModifiable = isModifiable;
         }
     }
 }
