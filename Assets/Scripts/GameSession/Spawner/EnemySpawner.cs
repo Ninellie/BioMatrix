d48673e54c6b7a9ac9dc71d6f6e54cdee197a955
Ninellie <a.pawlow2009@ya.ru>
2023-09-11 00:52:35 +0300

    IMPROVE: Better enemy spawning control

--- Assets/Scripts/GameSession/Spawner/EnemySpawner.cs
+++ Assets/Scripts/GameSession/Spawner/EnemySpawner.cs
@@ -1,26 +1,47 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Assets.Scripts.EntityComponents.Stats;
 using Assets.Scripts.EntityComponents.UnitComponents.EnemyComponents;
 using Assets.Scripts.EntityComponents.UnitComponents.Movement;
 using Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Assets.Scripts.GameSession.Spawner
 {
+
+    [Serializable]
+    public class EnemySpawnData
+    {
+        public GameObject enemyPrefab;
+        public AnimationCurve spawnWeightCurve;
+        public float currentWeight;
+    }
+
+    [Serializable]
+    public class InspectorSpawnData
+    {
+        public string enemyName;
+        public float currentSpawnWeight;
+
+        public InspectorSpawnData(string enemyName, float currentSpawnWeight)
+        {
+            this.enemyName = enemyName;
+            this.currentSpawnWeight = currentSpawnWeight;
+        }
+    }
+
     public class EnemySpawner : MonoBehaviour
     {
-        [Header("Set in Inspector")]
-        public GameObject[] enemyPrefabs;
+        [SerializeField] private EnemySpawnData[] _enemiesSpawnData;
         [SerializeField] private int _secondsBetweenWaves;
-        private readonly System.Random _random = new();
         private readonly Grouping _grouping = new();
         private readonly Rarity _rarity = new();
-        private readonly Circle _circle = new Circle();
+        private readonly Circle _circle = new();
         private GameObject _player;
         private EnemyWaveProperties _enemyWaveProperties;
         private const int DefaultComplicationValue = 60;
+
         private int TimerBonus
         {
             get
@@ -49,6 +70,10 @@ namespace Assets.Scripts.GameSession.Spawner
             InvokeRepeating(nameof(SpawnNormalWave), _secondsBetweenWaves, _secondsBetweenWaves);
         }
 
+        private void SpawnFirstWave() => SpawnWave(WaveType.First);
+
+        private void SpawnNormalWave() => SpawnWave(WaveType.Normal);
+
         private void SpawnWave(WaveType waveType)
         {
             if(_player is null) return;
@@ -57,15 +82,66 @@ namespace Assets.Scripts.GameSession.Spawner
             var playerPosition = (Vector2)_player.transform.position;
             var waveSize = _enemyWaveProperties.GetSize(waveType);
             var spawn = CreateWave(waveSize);
+            if (spawn is null) return;
             var mode = _grouping.GetRandomMode();
 
             PlaceEnemies(spawn, mode, playerPosition);
             PrepareEnemies(spawn, playerPosition);
         }
 
-        private void SpawnFirstWave() => SpawnWave(WaveType.First);
+        private GameObject[] CreateWave(int waveSize)
+        {
+            //var spawn = GetEnemyList(waveSize, currentEnemyPrefabs);
+            var spawn = GetWeighedEnemyListByTime(waveSize);
+            if (spawn is null) return null;
+            if (spawn.Count == 0) return null;
+            var spawnedEnemies = new List<GameObject>();
+            foreach (var enemy in spawn)
+            {
+                if (enemy is null) return null;
+                var e = Instantiate(enemy);
+                spawnedEnemies.Add(e);
+            }
+            return spawnedEnemies.ToArray();
+        }
 
-        private void SpawnNormalWave() => SpawnWave(WaveType.Normal);
+        private List<GameObject> GetWeighedEnemyListByTime(int numberOfEnemies)
+        {
+            var currentTime = Time.timeSinceLevelLoad;
+            var weightSum = 0f;
+
+            foreach (var enemySpawnData in _enemiesSpawnData)
+            {
+                var currentEnemyWeight = enemySpawnData.spawnWeightCurve.Evaluate(currentTime);
+                enemySpawnData.currentWeight = currentEnemyWeight;
+                if (!(currentEnemyWeight > 0)) continue;
+                weightSum += currentEnemyWeight;
+            }
+
+            var enemyList = new List<GameObject>(numberOfEnemies);
+
+            // Отбор множества врагов по их весу
+            for (int i = 0; i < enemyList.Capacity; i++)
+            {
+                enemyList.Add(GetWeighedEnemyByTime(weightSum, currentTime));
+            }
+
+            return enemyList;
+        }
+
+        private GameObject GetWeighedEnemyByTime(float weightSum, float time)
+        {
+            var next = Random.Range(0, weightSum);
+            var limit = 0f;
+            foreach (var enemySpawnData in _enemiesSpawnData)
+            {
+                var currentEnemyWeight = enemySpawnData.spawnWeightCurve.Evaluate(time);
+                limit += currentEnemyWeight;
+                if (next < limit) return enemySpawnData.enemyPrefab;
+            }
+            Debug.LogWarning("No enemies available for current time");
+            return null;
+        }
 
         private bool IsSpawnBlocked()
         {
@@ -74,26 +150,15 @@ namespace Assets.Scripts.GameSession.Spawner
             return enemyCount >= maxEnemies;
         }
 
-        private void PlaceEnemies(GameObject[] enemies, GroupingMode mode, Vector2 playerPosition)
+        private void PlaceEnemies(IReadOnlyList<GameObject> enemies, GroupingMode mode, Vector2 playerPosition)
         {
             float padding = 0;
             if (mode == GroupingMode.Group)
                 padding = enemies.Sum(enemy => enemy.GetComponent<CircleCollider2D>().radius) / 2;
 
-            var positions = _circle.GetPositions(enemies.Length, mode, playerPosition, padding);
-            for (int i = 0; i < enemies.Length; i++)
+            var positions = _circle.GetPositions(enemies.Count, mode, playerPosition, padding);
+            for (int i = 0; i < enemies.Count; i++)
                 enemies[i].transform.position = positions[i];
-        }
-
-        private GameObject[] CreateWave(int waveSize)
-        {
-            var spawn = GetEnemyList(waveSize, enemyPrefabs);
-            var spawnedEnemies = new GameObject[waveSize];
-
-            for (var i = 0; i < spawn.Count; i++)
-                spawnedEnemies[i] = Instantiate(spawn[i]);
-
-            return spawnedEnemies;
         }
 
         private void PrepareEnemies(IEnumerable<GameObject> enemies, Vector2 playerPosition)
@@ -108,42 +173,12 @@ namespace Assets.Scripts.GameSession.Spawner
 
         private void PrepareEnemy(Enemy enemy, Vector2 playerPosition)
         {
-            //var levelUpBonus = TimerBonus;
+            var levelUpBonus = TimerBonus;
+            Debug.LogWarning(levelUpBonus);
             enemy.SetRarity(_rarity.GetRandomRarity());
             //enemy.LevelUp(levelUpBonus);
             if (enemy.GetEnemyType() != EnemyType.AboveView) return;
             enemy.LookAt2D(playerPosition);
-        }
-
-        private List<GameObject> GetEnemyList(int numberOfEnemies, IEnumerable<GameObject> enemyTypes)
-        {
-            var enemyList = enemyTypes.ToList();
-            List<GameObject> selectedEnemies = new();
-
-            for (var i = 0; i < numberOfEnemies; i++)
-            {
-                var randomEnemyFromList = GetRandomEnemyFromList(enemyList);
-                selectedEnemies.Add(randomEnemyFromList);
-            }
-            return selectedEnemies;
-        }
-
-        private GameObject GetRandomEnemyFromList(List<GameObject> enemyList)
-        {
-            var sum = enemyList.Sum(enemy =>
-                (int)enemy.GetComponent<StatList>().GetPresetStatValue(StatName.SpawnWeight));
-
-            var next = _random.Next(sum);
-            var limit = 0;
-            foreach (var enemy in enemyList)
-            {
-                limit += (int)enemy.GetComponent<StatList>().GetPresetStatValue(StatName.SpawnWeight);
-                if (next < limit)
-                {
-                    return enemy;
-                }
-            }
-            throw new InvalidOperationException("");
         }
     }
 }
