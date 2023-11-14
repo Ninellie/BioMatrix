618ac809cb14c170b08c01cb1ad8b5f976fcc564
Ninellie <a.pawlow2009@ya.ru>
2023-11-15 02:42:10 +0300

    Added KillCounter class - separate class wich listening onEnemyKill gameEvent

--- Assets/KillCounter.cs
+++ Assets/KillCounter.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class KillCounter : MonoBehaviour
 {
     [SerializeField] private IntReference _playerKills;
+    [SerializeField] private bool _resetOnAwake;
+
+    private void Awake()
+    {
+        _playerKills.Value = 0;
+    }
 
     public void IncreaseKillCounter()
     {
