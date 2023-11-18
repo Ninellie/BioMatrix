986dd67165f1a8337a2f6ac88fba9c04d35452f3
Ninellie <a.pawlow2009@ya.ru>
2023-11-18 04:54:51 +0300

    Added Vector2 variable, reference and drawer

---
+++ Assets/Scripts/Core/Variables/References/Vector2Reference.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Variables.References
+{
+    [Serializable]
+    public class Vector2Reference
+    {
+        public bool useConstant;
+        public Vector2 constantValue;
+        public Vector2Variable variable;
+
+        public Vector2Reference()
+        { }
+
+        public Vector2Reference(Vector2 value)
+        {
+            useConstant = true;
+            constantValue = value;
+        }
+
+        public Vector2 Value => useConstant ? constantValue : variable.value;
+
+        public static implicit operator Vector2(Vector2Reference reference)
+        {
+            return reference.Value;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Variables/Vector2Variable.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Variables
+{
+    [CreateAssetMenu(fileName = "New Vector2 Variable", menuName = "Variables/Vector2", order = 51)]
+    public class Vector2Variable : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [Multiline]
+        public string developerDescription = "";
+#endif
+        public Vector2 value;
+
+        public void SetValue(Vector2 value)
+        {
+            this.value = value;
+        }
+
+        public void SetValue(Vector2Variable value)
+        {
+            this.value = value.value;
+        }
+
+        public void ApplyChange(Vector2 amount)
+        {
+            value += amount;
+        }
+
+        public void ApplyChange(Vector2Variable amount)
+        {
+            value += amount.value;
+        }
+    }
+}
