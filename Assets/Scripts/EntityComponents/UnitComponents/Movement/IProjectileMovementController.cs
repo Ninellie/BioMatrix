3393c71ae1d2cd98ad812d698c13f495af6d6571
Ninellie <a.pawlow2009@ya.ru>
2023-11-22 17:57:14 +0300

    Cleaning: IProjectileMovement interface was deleted

--- Assets/Scripts/EntityComponents/UnitComponents/Movement/IProjectileMovementController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Assets.Scripts.EntityComponents.UnitComponents.Movement
-{
-    public interface IProjectileMovementController : IMovementController
-    {
-        public void SetDirection(Vector2 direction);
-    }
-}
