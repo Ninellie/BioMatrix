ba8a6c7eecf3d8950c951998c21ed9885aabefd1
Ninellie <a.pawlow2009@ya.ru>
2023-11-22 23:31:44 +0300

    Added ProjectileCollisionHandler and ProjectileSizeController

---
+++ Assets/Scripts/EntityComponents/UnitComponents/ProjectileComponents/ProjectileCollisionHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Scripts.EntityComponents.UnitComponents.ProjectileComponents
+{
+    public class ProjectileCollisionHandler : MonoBehaviour
+    {
+        [SerializeField] private string _enemyTag;
+        [SerializeField] private UnityEvent _onBecomeInvisible;
+        [SerializeField] private UnityEvent<int> _onCollision;
+
+        private void OnCollisionEnter2D(Collision2D collision2D)
+        {
+            var otherCollider2D = collision2D.collider;
+            if (!otherCollider2D.gameObject.CompareTag(_enemyTag)) return;
+            _onCollision.Invoke(1);
+        }
+
+        private void OnBecameInvisible()
+        {
+            _onBecomeInvisible.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/EntityComponents/UnitComponents/ProjectileComponents/ProjectileSizeController.cs
@@ -0,0 +1,26 @@
+using Assets.Scripts.Core.Variables.References;
+using UnityEngine;
+
+namespace Assets.Scripts.EntityComponents.UnitComponents.ProjectileComponents
+{
+    public class ProjectileSizeController : MonoBehaviour
+    {
+        [SerializeField] private FloatReference _size;
+        [SerializeField] private TrailRenderer _trail;
+        [SerializeField] private CircleCollider2D _circleCollider;
+        [SerializeField] private Transform _transform;
+
+        private void Awake()
+        {
+            if (_trail == null) _trail = GetComponent<TrailRenderer>();
+            if(_circleCollider == null) _circleCollider = GetComponent<CircleCollider2D>();
+            if (_transform == null) _transform = transform;
+        }
+
+        private void Start()
+        {
+            _transform.localScale = new Vector3(_size, _size, 1);
+            _trail.startWidth = _circleCollider.radius * 2 * _size;
+        }
+    }
+}
