638f761774df1264c38efa84d23edd4b470318ff
Ninellie <a.pawlow2009@ya.ru>
2023-11-24 21:44:12 +0300

    Refactoring: counter and bar indicators

--- Assets/Scripts/View/Bar.cs
+++ Assets/Scripts/View/Bar.cs
@@ -5,14 +5,14 @@ using UnityEngine.UI;
 
 namespace Assets.Scripts.View
 {
-    [AddComponentMenu("UI/Indicator/Reserve bar")]
-    public class ReserveBar : ReserveCounter
+    [AddComponentMenu("UI/Indicator/Bar")]
+    public class Bar : Counter
     {
         [Space]
         [Header("Bar settings")]
         [SerializeField] private BarDisplayFormat _barDisplayFormat;
         [SerializeField] private float _limit;
-        [SerializeField] [Range(0, 1)] private float _valueStepPerResourceUnit;
+        [SerializeField] [Range(0, 1)] private float _valueStepPerUnit;
         [Space]
         [Header("background")]
         [SerializeField] private Image _backgroundImage;
@@ -29,6 +29,13 @@ namespace Assets.Scripts.View
             SetBackgroundImageColor(_backgroundColor);
         }
 
+        private void OnValidate()
+        {
+            SetValueImageColor(_valueColor);
+            SetBackgroundImageColor(_backgroundColor);
+            UpdateCounter();
+        }
+
         public override void UpdateCounter()
         {
             UpdateBarValue();
@@ -37,11 +44,13 @@ namespace Assets.Scripts.View
 
         private void UpdateBarValue()
         {
+            var percentValue = GetPercentCurrentValue();
+
             var value = _barDisplayFormat switch
             {
-                BarDisplayFormat.Percent => GetPercentCurrentValue() / 100f,
-                BarDisplayFormat.LimitedToNumber => Mathf.Min(_reserve.Value, _limit),
-                BarDisplayFormat.Unlimited => _reserve.Value * _valueStepPerResourceUnit,
+                BarDisplayFormat.Percent => percentValue > 0 ? percentValue / 100f : 0,
+                BarDisplayFormat.LimitedToNumber => Mathf.Min(_value.Value, _limit),
+                BarDisplayFormat.Unlimited => _value.Value * _valueStepPerUnit,
                 _ => throw new ArgumentOutOfRangeException()
             };
             _sliderRectTransform.anchorMax = new Vector2(value, 1);
---
+++ Assets/Scripts/View/Counter.cs
@@ -0,0 +1,169 @@
+using System;
+using Assets.Scripts.Core.Variables.References;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.View
+{
+    public enum CounterFormat
+    {
+        LabelCurrentSlashMax,
+        CurrentSlashMax,
+        LabelCurrentOnly,
+        CurrentOnly,
+        LabelOnly,
+        DoNotShow
+    }
+
+    public enum ValueFormat
+    {
+        D,
+        D1,
+        D2,
+        D3
+    }
+
+    public enum PercentFormat
+    {
+        Flat,
+        Sigh
+    }
+
+    [AddComponentMenu("UI/Indicator/Сounter")]
+    public class Counter : MonoBehaviour
+    {
+        [Header("Reserve settings")]
+        [Header("Label")]
+        [SerializeField] private string _counterName = "Simple counter";
+        [Space]
+        [Header("Value settings")]
+        [SerializeField] protected IntReference _value;
+        [SerializeField] protected FloatReference _maximumValue;
+        [Space]
+        [Header("Counter text settings")]
+        [SerializeField] private CounterFormat _counterFormat;
+        [SerializeField] private ValueFormat _valueFormat;
+        [SerializeField] private bool _inPercent;
+        [SerializeField] private PercentFormat _percentFormat;
+        [Space]
+        [Header("Counter text")]
+        [SerializeField] private TMP_Text _text;
+
+        public virtual void UpdateCounter()
+        {
+            UpdateLabel();
+        }
+
+        private void OnValidate()
+        {
+            UpdateCounter();
+        }
+
+        private void UpdateLabel()
+        {
+            var currentValue = GetValue();
+
+            var textToSet = _counterFormat switch
+            {
+                CounterFormat.LabelCurrentSlashMax => $"{_counterName} {currentValue}/{GetMaxValue()}",
+                CounterFormat.CurrentSlashMax => $"{currentValue}/{GetMaxValue()}",
+                CounterFormat.LabelCurrentOnly => $"{_counterName} {currentValue}",
+                CounterFormat.CurrentOnly => $"{currentValue}",
+                CounterFormat.LabelOnly => $"{_counterName}",
+                CounterFormat.DoNotShow => $"",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            _text.SetText(textToSet);
+        }
+
+        protected int GetPercentCurrentValue()
+        {
+            if (!_maximumValue.useConstant && _maximumValue.variable == null)
+            {
+                Debug.unityLogger.LogWarning($"UI", $"You are using the maximum value as a variable in the counter, but you have not set the variable in inspector.", this);
+                return 0;
+            }
+            if (!_value.useConstant && _value.variable == null)
+            {
+                Debug.unityLogger.LogWarning($"UI", $"You are using the value as a variable in the counter, but you have not set the variable in inspector.", this);
+                return 0;
+            }
+            if (_value.Value == 0) return 0;
+            if (_maximumValue.Value == 0) return 0;
+            var percent = _maximumValue.Value / 100;
+            var currentPercent = _value.Value / percent;
+            return (int)currentPercent;
+        }
+
+        private string GetMaxValue()
+        {
+            if (_maximumValue == null) return "";
+            if (!_maximumValue.useConstant && _maximumValue.variable == null)
+            {
+                Debug.unityLogger.LogWarning($"UI", $"You are using the maximum value as a variable in the counter, but you have not set the variable in inspector.", this);
+                return "";
+            }
+            if (_inPercent)
+            {
+                var maxPercentValue = "100";
+                if (_percentFormat == PercentFormat.Sigh)
+                {
+                    maxPercentValue += "%";
+                }
+                return maxPercentValue;
+            }
+
+            var intMaxValue = (int)_maximumValue.Value;
+
+            var maxValue = _valueFormat switch
+            {
+                ValueFormat.D => $"{intMaxValue:D}",
+                ValueFormat.D1 => $"{intMaxValue:D1}",
+                ValueFormat.D2 => $"{intMaxValue:D2}",
+                ValueFormat.D3 => $"{intMaxValue:D3}",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            return maxValue;
+        }
+
+        private string GetValue()
+        {
+            if (!_value.useConstant && _value.variable == null)
+            {
+                Debug.unityLogger.LogWarning($"UI", $"You are using the value as a variable in the counter, but you have not set the variable in inspector.", this);
+                return "";
+            }
+
+            var currentIntValue = 0;
+
+            if (_value != null)
+            {
+                if (_inPercent)
+                { 
+                    currentIntValue = GetPercentCurrentValue();
+                }
+                else
+                {
+                    currentIntValue = _value.Value;
+                }
+            }
+
+            var currentValue = _valueFormat switch
+            {
+                ValueFormat.D => $"{currentIntValue:D}",
+                ValueFormat.D1 => $"{currentIntValue:D1}",
+                ValueFormat.D2 => $"{currentIntValue:D2}",
+                ValueFormat.D3 => $"{currentIntValue:D3}",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            if (_inPercent && _percentFormat == PercentFormat.Sigh)
+            {
+                currentValue += "%";
+            }
+
+            return currentValue;
+        }
+    }
+}
