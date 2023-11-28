ec7721da4a8cdba8a4a56250ff0c58d6599c22ac
Ninellie <a.pawlow2009@ya.ru>
2023-11-28 17:49:10 +0300

    Refactoring: the logic of flashing the sprite and changing layers has been separated, thanks to the division of the component into two. Added sprite flash controller

---
+++ Assets/Scripts/EntityComponents/SpriteFlashController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace Assets.Scripts.EntityComponents
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteFlashController : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private Color _originalColor;
+        [Header("Settings")]
+        [SerializeField] private float _baseFlashingDuration = 1f;
+        [SerializeField] private float _flashDuration = 0.1f;
+        [SerializeField] private float _flashAlpha = 0.3f;
+
+        [SerializeField] private float _flashingStopwatch;
+        [SerializeField] private bool _isFlashing = false;
+
+        private void Awake()
+        {
+            if (_spriteRenderer == null) _spriteRenderer = GetComponent<SpriteRenderer>();
+            _originalColor = _spriteRenderer.color;
+            _isFlashing = false;
+        }
+
+        private void Update()
+        {
+            if (!_isFlashing) return;
+            _flashingStopwatch -= Time.time;
+            var t = Mathf.PingPong(_flashingStopwatch / _flashDuration, 1f);
+            var alpha = Mathf.Lerp(1f, _flashAlpha, t);
+            _spriteRenderer.color = new Color(_originalColor.r, _originalColor.g, _originalColor.b, alpha);
+        }
+
+        public void StartFlashing()
+        {
+            _isFlashing = true;
+            _flashingStopwatch = _baseFlashingDuration;
+            Invoke(nameof(StopFlashing), _baseFlashingDuration);
+        }
+
+        public void StopFlashing()
+        {
+            _isFlashing = false;
+            _spriteRenderer.color = Color.white;
+            _flashingStopwatch = 0f;
+        }
+    }
+}
