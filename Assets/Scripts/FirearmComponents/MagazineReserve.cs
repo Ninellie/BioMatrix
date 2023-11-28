19e181769f978e12a7ca10ec31defe84bba527b5
Ninellie <a.pawlow2009@ya.ru>
2023-11-28 17:47:49 +0300

    Improvements: magazine reserve component

--- Assets/Scripts/FirearmComponents/MagazineReserve.cs
+++ Assets/Scripts/FirearmComponents/MagazineReserve.cs
@@ -4,39 +4,37 @@ using UnityEngine;
 
 namespace Assets.Scripts.FirearmComponents
 {
-    // Герой жмёт на кнопку стрельбы
-    // Отсылается ивент стрельбы
-    // Оружие начинает реагировать на ивент с DoAction
-    // Выстрел
-    // Минус 1 патрон
-    // Подождать дилей скорости атаки
-    // Если в магазине не осталось патронов, то запустить перезарядку
-
     public class MagazineReserve : MonoBehaviour
     {
         [field: SerializeField] public bool OnReload { get; private set; }
         [Space]
         [SerializeField] private IntReference _value;
+        [SerializeField] private bool _fillOnStart;
         [Space]
         [SerializeField] private GameEvent _onReloadStart;
         [SerializeField] private GameEvent _onReloadComplete;
         [Space]
         [SerializeField] private FloatReference _reloadSpeedStat;
         [SerializeField] private FloatReference _maxMagazineCapacity;
+        [SerializeField] private IntReference _ammoPerShoot;
 
         public int Value => _value.Value;
 
+        private void Start()
+        {
+            if (!_fillOnStart) return;
+            Fill();
+        }
+
         public void Pop()
         {
+            if (_value == 0)
+            {
+                InitiateReload();
+                return;
+            }
             if (OnReload) return;
-            if (_value.useConstant)
-            {
-                _value.constantValue--;
-            }
-            else
-            {
-                _value.variable.ApplyChange(-1);
-            }
+            ApplyChange(-_ammoPerShoot);
 
             if (_value == 0)
             {
@@ -68,6 +66,14 @@ namespace Assets.Scripts.FirearmComponents
             OnReload = false;
             Fill();
             _onReloadComplete.Raise();
+        }
+
+        private void ApplyChange(int amount)
+        {
+            if (_value.useConstant)
+                _value.constantValue += amount;
+            else
+                _value.variable.ApplyChange(amount);
         }
 
         private void Fill()
