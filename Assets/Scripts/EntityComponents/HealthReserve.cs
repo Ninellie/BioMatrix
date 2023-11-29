2ed5106247ececd5de35b0ac6cb20118bd12acdf
Ninellie <a.pawlow2009@ya.ru>
2023-11-29 20:27:04 +0300

    Refactoring: health reserve

--- Assets/Scripts/EntityComponents/HealthReserve.cs
+++ Assets/Scripts/EntityComponents/HealthReserve.cs
@@ -7,20 +7,25 @@ namespace Assets.Scripts.EntityComponents
 {
     public class HealthReserve : MonoBehaviour
     {
+        [Header("Settings")]
         [SerializeField] private bool _disableObjectOnEmpty;
+        [SerializeField] private GameObjectReference _selfGameObject;
         [SerializeField] private bool _refillOnStart;
         [SerializeField] private IntReference _currentHealth;
         [SerializeField] private StatReference _maximumHealth;
         [SerializeField] private int _edgeValue;
+        [Header("Events")]
+        [SerializeField] private GameEvent _onEmpty;
+        [SerializeField] private UnityEvent _onEmptyUnityEvent;
+
+        [SerializeField] private GameEvent _onEdge;
+
+        [SerializeField] private GameEvent _onDecrease;
+        [SerializeField] private UnityEvent<int> _onDecreaseUnityEvent;
+
         public int Value => _currentHealth.Value;
         public bool IsEmpty => _currentHealth.Value == 0;
         public bool IsFull => _currentHealth.Value == (int)_maximumHealth.Value;
-
-        [SerializeField] private GameEvent _onEmpty;
-        [SerializeField] private UnityEvent _onEmptyUnityEvent;
-        [SerializeField] private GameEvent _onEdge;
-        [SerializeField] private GameEvent _onDecrease;
-        [SerializeField] private UnityEvent<int> _onDecreaseUnityEvent;
 
         private void Start()
         {
@@ -93,6 +98,10 @@ namespace Assets.Scripts.EntityComponents
                 {
                     _onEmpty.Raise();
                     _onEmptyUnityEvent.Invoke();
+                    if (_disableObjectOnEmpty)
+                    {
+                        _selfGameObject.Value.SetActive(false);
+                    }
                 }
             }
         }
