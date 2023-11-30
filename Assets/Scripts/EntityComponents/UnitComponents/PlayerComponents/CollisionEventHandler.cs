f5b545aa4496e952c46237f5964dac390cfb4481
Ninellie <a.pawlow2009@ya.ru>
2023-11-30 23:30:00 +0300

    Improvements: collision event handler now can handle onCollisionStay2D events, can reverse a distance responce and can use separate point of collision object via Vector2Reference

--- Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/CollisionEventHandler.cs
+++ Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/CollisionEventHandler.cs
@@ -8,11 +8,16 @@ namespace Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents
     {
         [Header("Inner components")]
         [SerializeField] private Transform _transform;
-        [Header("Settings")]
-        [SerializeField] private string _otherTag;
-        [SerializeField] private FloatReference _otherKnockbackPower;
+        [Header("Collision object")]
+        [SerializeField] private string _tag;
+        [SerializeField] private FloatReference _distanceLength;
+        [SerializeField] private bool _useSeparatePosition;
+        [SerializeField] private Vector2Reference _position;
         [Header("Response")]
+        [Tooltip("Direction to collision object")]
+        [SerializeField] private bool _inverseDirection;
         [SerializeField] private UnityEvent<Vector2> _onCollisionEnter2D;
+        [SerializeField] private UnityEvent<Vector3> _onCollisionStay2D;
 
         private void Awake()
         {
@@ -21,10 +26,26 @@ namespace Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents
 
         private void OnCollisionEnter2D(Collision2D collision2D)
         {
-            if (collision2D.collider.tag != _otherTag) return;
-            var direction = (_transform.position - collision2D.gameObject.transform.position).normalized;
-            var force = direction * _otherKnockbackPower;
-            _onCollisionEnter2D.Invoke(force);
+            if (collision2D.collider.tag != _tag) return;
+            var distance = GetDistance(collision2D);
+            _onCollisionEnter2D.Invoke(distance);
+        }
+
+        private void OnCollisionStay2D(Collision2D collision2D)
+        {
+            if (collision2D.collider.tag != _tag) return;
+            var distance = GetDistance(collision2D);
+            _onCollisionStay2D.Invoke(distance);
+        }
+
+        private Vector3 GetDistance(Collision2D collision2D)
+        {
+            Vector3 collisionObjectPosition = _useSeparatePosition ?
+                _position.Value : collision2D.gameObject.transform.position;
+            var direction = (_transform.position - collisionObjectPosition).normalized;
+            if (_inverseDirection) direction = -direction;
+            var force = direction * _distanceLength;
+            return force;
         }
     }
 }
