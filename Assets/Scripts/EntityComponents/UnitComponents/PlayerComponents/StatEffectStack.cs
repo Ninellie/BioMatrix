60f3f8fca526a0faa27b068b265985273ea51e78
Ninellie <a.pawlow2009@ya.ru>
2023-12-09 02:44:30 +0300

    Added Stat Effect Stack component

---
+++ Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/StatEffectStack.cs
@@ -0,0 +1,28 @@
+using Assets.Scripts.SourceStatSystem;
+using UnityEngine;
+
+namespace Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents
+{
+    [ExecuteInEditMode]
+    public class StatEffectStack : MonoBehaviour
+    {
+        [SerializeField] private StatSources _list;
+        [SerializeField] private StatSourceData[] _statSources;
+
+        private void OnEnable()
+        {
+            foreach (var source in _statSources)
+            {
+                _list.AddStatSource(source);
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var source in _statSources)
+            {
+                _list.RemoveStatSource(source);
+            }
+        }
+    }
+}
