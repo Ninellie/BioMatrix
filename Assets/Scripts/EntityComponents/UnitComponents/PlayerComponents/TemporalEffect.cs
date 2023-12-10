44ba4fb9eb7b349df3ea9cccc39bb9753ff22c19
Ninellie <a.pawlow2009@ya.ru>
2023-12-10 03:04:25 +0300

    Refactoring: added Temporal effect component

---
+++ Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/TemporalEffect.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents
+{
+    public class TemporalEffect : MonoBehaviour
+    {
+        [SerializeField] private StackPool _pool;
+        [SerializeField] [Range(0, 50)] private float _duration;
+
+        public void TryAdd()
+        {
+            CancelInvoke(nameof(DisableAll));
+            _pool.TryAdd();
+            Invoke(nameof(DisableAll), _duration);
+        }
+
+        private void DisableAll()
+        {
+            _pool.DisableAll();
+        }
+
+        private void TryRelease()
+        {
+            _pool.TryRelease();
+        }
+    }
+}
