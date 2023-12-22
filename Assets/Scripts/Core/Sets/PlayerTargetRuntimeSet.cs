7c7ff1f03748d7880f72f6dd55c5f866385daa65
Ninellie <a.pawlow2009@ya.ru>
2023-12-22 04:33:02 +0300

    Refactoring: some logic moved from Firearm class to Runtime Set class

--- Assets/Scripts/Core/Sets/PlayerTargetRuntimeSet.cs
+++ Assets/Scripts/Core/Sets/PlayerTargetRuntimeSet.cs
@@ -5,5 +5,25 @@ namespace Assets.Scripts.Core.Sets
 {
     [CreateAssetMenu(fileName = "New Player Target Set", menuName = "Sets/Player target", order = 51)]
     public class PlayerTargetRuntimeSet : RuntimeSet<PlayerTarget>
-    { }
+    {
+        public PlayerTarget GetNearestToCenterInCircle(Vector2 center, float radius)
+        {
+            var distanceToNearestTarget = Mathf.Infinity;
+            PlayerTarget nearestTarget = null;
+            foreach (var target in items)
+            {
+                var distance = Vector2.Distance(center, target.transform.position);
+                if (!(distance < radius)) continue;
+                if (!(distance < distanceToNearestTarget)) continue;
+                distanceToNearestTarget = distance;
+                nearestTarget = target;
+            }
+            return nearestTarget;
+        }
+
+        public PlayerTarget GetNearestToPosition(Vector2 position)
+        {
+            return GetNearestToCenterInCircle(position, Mathf.Infinity);
+        }
+    }
 }
