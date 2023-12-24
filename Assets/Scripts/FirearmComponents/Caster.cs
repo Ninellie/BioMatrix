b882381369e913284d63144e5cc87310ea76d345
Ninellie <a.pawlow2009@ya.ru>
2023-12-24 23:06:34 +0300

    Added caster component

---
+++ Assets/Scripts/FirearmComponents/Caster.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using Assets.Scripts.Core.Variables.References;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Scripts.FirearmComponents
+{
+    public enum CastMode
+    {
+        BeforeDelay,
+        AfterDelay
+    }
+
+    /// <summary>
+    /// Caster Component invokes a cast event when enabled at given cast speed
+    /// </summary>
+    public class Caster : MonoBehaviour
+    {
+        [SerializeField] private FloatReference _castsPerSecond;
+        [SerializeField] private CastMode _mode;
+        [SerializeField] private UnityEvent _onCast;
+
+        private void OnEnable()
+        {
+            StartCoroutine(Co_Cast());
+        }
+
+        private void OnDisable()
+        {
+            StopAllCoroutines();
+        }
+
+        private IEnumerator Co_Cast()
+        {
+            while (true)
+            {
+                switch (_mode)
+                {
+                    case CastMode.BeforeDelay:
+                        yield return new WaitForSeconds(1f / _castsPerSecond);
+                        _onCast.Invoke();
+                        break;
+                    case CastMode.AfterDelay:
+                        yield return new WaitForSeconds(1f / _castsPerSecond);
+                        _onCast.Invoke();
+                        break;
+                }
+            }
+        }
+    }
+}
