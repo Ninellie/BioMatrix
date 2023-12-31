dc8bc54b47fdfd60e6bb7284d265b781e2015ff1
Ninellie <a.pawlow2009@ya.ru>
2023-12-31 06:31:13 +0300

    Added LifeTimer conponent - a simple tool for checking how long does it take from Enable to Disable for an object

---
+++ Assets/Scripts/EntityComponents/LifeTimer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Scripts.EntityComponents
+{
+    public class LifeTimer : MonoBehaviour
+    {
+        public float lifeTime;
+
+        public UnityEvent onEnable;
+        public UnityEvent onDisable;
+
+        private void OnEnable()
+        {
+            lifeTime = 0;
+            onEnable.Invoke();
+        }
+
+        private void OnDisable()
+        {
+            Debug.Log($"Life time: {lifeTime}");
+            onDisable.Invoke();
+        }
+
+        private void Update()
+        {
+            lifeTime += Time.deltaTime;
+        }
+    }
+}
