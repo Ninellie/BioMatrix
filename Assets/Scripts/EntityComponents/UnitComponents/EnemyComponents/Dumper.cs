f8ec73660df9bc359ac427ddffa4ea7c9c28ab2c
Ninellie <a.pawlow2009@ya.ru>
2024-01-06 20:28:20 +0300

    Added Dumper Component

--- Assets/Scripts/EntityComponents/UnitComponents/EnemyComponents/Dumper.cs
+++ Assets/Scripts/EntityComponents/UnitComponents/EnemyComponents/Dumper.cs
@@ -5,12 +5,11 @@ namespace Assets.Scripts.EntityComponents.UnitComponents.EnemyComponents
     public class Dumper : MonoBehaviour
     {
         [SerializeField] private GameObject _drop;
-        public bool DropOnDisable { get; set; }
+        [field: SerializeField] public bool CanDrop { get; set; }
 
         public void DropBonus()
         {
-            _drop.SetActive(true);
-            _drop.transform.SetParent(null, true);
+            Instantiate(_drop, transform.position, Quaternion.identity);
         }
     }
 }
