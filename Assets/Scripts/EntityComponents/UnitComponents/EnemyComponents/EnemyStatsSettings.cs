4e0a8bbebfb0277a8495c08dd881844a4498e706
Ninellie <a.pawlow2009@ya.ru>
2024-01-20 09:44:23 +0300

    deleted old unused scripts

--- Assets/Scripts/EntityComponents/UnitComponents/EnemyComponents/EnemyStatsSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace Assets.Scripts.EntityComponents.UnitComponents.EnemyComponents
-{
-    //public class EnemyStatsSettings : UnitStatsSettings
-    //{
-    //    public float spawnWeight;
-    //}
-}
--- Assets/Scripts/EntityComponents/UnitComponents/EnemyComponents/IDamageTaker.cs
+++
@@ -1,8 +0,0 @@
-namespace Assets.Scripts.EntityComponents.UnitComponents.EnemyComponents
-{
-    public interface IDamageTaker
-    {
-        //bool IsAlive { get; }
-        //void TakeDamage(int damageAmount);
-    }
-}
