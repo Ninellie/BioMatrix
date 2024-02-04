6a369aee29e7794f09af28ceb39861cca1b1dae7
Ninellie <a.pawlow2009@ya.ru>
2024-02-04 22:57:17 +0300

    Added Projectile Pool component

---
+++ Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/ProjectilePool.cs
@@ -0,0 +1,8 @@
+using Assets.Scripts.EntityComponents.UnitComponents.Movement;
+
+namespace Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents
+{
+    public class ProjectilePool : Pool<ProjectileMovementController>
+    {
+    }
+}
