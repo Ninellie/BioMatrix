c8456e6b455eb2d9a20c129c53db74bf2ba899aa
Ninellie <a.pawlow2009@ya.ru>
2024-02-15 17:08:53 +0300

    EnemyPool improves

--- Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/EnemyPool.cs
+++ Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/EnemyPool.cs
@@ -1,9 +1,15 @@
 using Assets.Scripts.GameSession.Spawner;
+using UnityEngine;
 
 namespace Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents
 {
     public class EnemyPool : Pool<EnemyData>
     {
         public EnemySpawnData spawnData;
+
+        public float GetWeigth()
+        {
+            return spawnData.spawnWeightCurve.Evaluate(Time.timeSinceLevelLoad);
+        }
     }
 }
