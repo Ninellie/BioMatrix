ffb16b480686d13ec69eb8d1ea001e4c595be54a
Ninellie <a.pawlow2009@ya.ru>
2024-02-20 02:26:32 +0300

    Added turret shooter component

--- Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/TurretShootDelaySetter.cs
+++ Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/TurretShootDelaySetter.cs
@@ -1,6 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using Assets.Scripts.Core.Sets;
-using Assets.Scripts.Core.Variables;
 using Assets.Scripts.Core.Variables.References;
+using Assets.Scripts.FirearmComponents;
 using UnityEngine;
 
 namespace Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents
@@ -8,12 +10,42 @@ namespace Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents
     public class TurretShootDelaySetter : MonoBehaviour
     {
         public FloatReference turretAttackSpeed;
-        public TransformRuntimeSet turrets;
-        public FloatVariable timeDelay;
+        public ShooterRuntimeSet turretShooters;
+        public float timeBetweenShoots;
 
-        private void FixedUpdate()
+
+        private void OnDisable()
         {
-            timeDelay.SetValue(turretAttackSpeed / 1f / turrets.items.Count);
+            StopAllCoroutines();
+        }
+
+        public void ReShoot()
+        {
+            StopAllCoroutines();
+
+            StartCoroutine(Co_Shoot());
+        }
+
+        public void Shoot()
+        {
+            StartCoroutine(Co_Shoot());
+        }
+
+        private IEnumerator Co_Shoot()
+        {
+            var shooterList = new List<Shooter>();
+
+            foreach (var turretShootersItem in turretShooters.items)
+            {
+                shooterList.Add(turretShootersItem);
+            }
+
+            foreach (var shooter in shooterList)
+            {
+                timeBetweenShoots = 1f / turretAttackSpeed / turretShooters.items.Count;
+                yield return new WaitForSeconds(timeBetweenShoots);
+                shooter.Shoot();
+            }
         }
     }
 }
