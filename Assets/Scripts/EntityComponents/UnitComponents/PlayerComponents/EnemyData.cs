7a81b1c05972e69f98c7bfded3d01fb14b21a441
Ninellie <a.pawlow2009@ya.ru>
2024-02-21 19:08:41 +0300

    EnemyData component improves

--- Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/EnemyData.cs
+++ Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/EnemyData.cs
@@ -8,5 +8,23 @@ namespace Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents
         public Transform transform;
         public Rigidbody2D rigidbody2D;
         public SpriteType spriteType;
+
+        private void Awake()
+        {
+            if (transform == null)
+            {
+                transform = this.gameObject.transform;
+            }
+
+            if (rigidbody2D == null)
+            {
+                rigidbody2D = GetComponent<Rigidbody2D>();
+            }
+
+            if (spriteType == null)
+            {
+                spriteType = GetComponent<SpriteType>();
+            }
+        }
     }
 }
