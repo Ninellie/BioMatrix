7f3a62292c2a5b2c533f094327c414489aeccb57
Ninellie <a.pawlow2009@ya.ru>
2024-03-01 17:21:10 +0300

    Added onBonusDrop event in dumper component

--- Assets/Scripts/EntityComponents/UnitComponents/EnemyComponents/Dumper.cs
+++ Assets/Scripts/EntityComponents/UnitComponents/EnemyComponents/Dumper.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Core.Events;
 using Assets.Scripts.Core.Variables.References;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ namespace Assets.Scripts.EntityComponents.UnitComponents.EnemyComponents
     {
         [SerializeField] private TransformPoolReference _drops;
         [field: SerializeField] public bool CanDrop { get; set; }
+        [SerializeField] private GameEvent _onBonusDrop;
 
         private void OnEnable()
         {
@@ -16,6 +18,10 @@ namespace Assets.Scripts.EntityComponents.UnitComponents.EnemyComponents
         public void DropBonus()
         {
             if (!CanDrop) return;
+            if (_onBonusDrop != null)
+            {
+                _onBonusDrop.Raise();
+            }
             var drop = _drops.Value.Get(); // TODO Поменять TransformPool на BoonDataPool или типа того
             drop.SetPositionAndRotation(transform.position, Quaternion.identity);
             drop.gameObject.GetComponent<TrailRenderer>().Clear();
