c93255d18acaa1f7b6f90b82814a7f8a4f6052a0
Ninellie <a.pawlow2009@ya.ru>
2024-03-08 17:37:09 +0300

    Added comment doc in AmmoData class

--- Assets/Scripts/FirearmComponents/AmmoData.cs
+++ Assets/Scripts/FirearmComponents/AmmoData.cs
@@ -3,6 +3,9 @@ using Assets.Scripts.EntityComponents.UnitComponents.PlayerComponents;
 
 namespace Assets.Scripts.FirearmComponents
 {
+    /// <summary>
+    /// Содержит данные о снаряде для магазина. Пул снарядов, а также вес выдачи в магазине.
+    /// </summary>
     [Serializable]
     public class AmmoData
     {
