6b35e16a37b80c69a7366be225cfb44fcc7c0c2b
Ninellie <a.pawlow2009@ya.ru>
2024-06-05 00:20:29 +0300

    Folder renaming, Player Input System prefab incapsulation refactoring

--- Assets/Scripts/Core/Input/LevelUpInputHandler.cs
+++ Assets/Scripts/Core/Input/LevelUpInputHandler.cs
@@ -1,34 +1,34 @@
+using Assets.Scripts.Core.Events;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 namespace Assets.Scripts.Core.Input
 {
     public class LevelUpInputHandler : MonoBehaviour
     {
-        public UnityEvent _onNextDeck;
-        public UnityEvent _onPreviousDeck;
-        public UnityEvent _onDeckAccept;
+        [SerializeField] private GameEvent _onNextDeck;
+        [SerializeField] private GameEvent _onPreviousDeck;
+        [SerializeField] private GameEvent _onDeckAccept;
 
         public void OnNextDeck(InputAction.CallbackContext context)
         {
             if (!context.action.WasReleasedThisFrame()) return;
             Debug.LogWarning($"On next deck");
-            _onNextDeck.Invoke();
+            _onNextDeck.Raise();
         }
 
         public void OnPreviousDeck(InputAction.CallbackContext context)
         {
             if (!context.action.WasReleasedThisFrame()) return;
             Debug.LogWarning($"On previous deck");
-            _onPreviousDeck.Invoke();
+            _onPreviousDeck.Raise();
         }
 
         public void OnDeckAccept(InputAction.CallbackContext context)
         {
             if (!context.action.WasReleasedThisFrame()) return;
             Debug.LogWarning($"On deck accept");
-            _onDeckAccept.Invoke();
+            _onDeckAccept.Raise();
         }
     }
 }
