4bec264beb714af3b90cda08b91ae15e7a15eade
Ninellie <a.pawlow2009@ya.ru>
2024-06-21 01:15:55 +0300

    Added better cursor

--- Assets/Scripts/UIScripts/Cursor/CursorController.cs
+++ Assets/Scripts/UIScripts/Cursor/CursorController.cs
@@ -4,10 +4,23 @@ namespace UIScripts.Cursor
 {
     public class CursorController : MonoBehaviour
     {
+        [SerializeField] private Texture2D aimCursor;
+        [SerializeField] private Texture2D reloadCursor;
+        
         private void Awake()
         {
-            
+            SetAimCursor();
             DontDestroyOnLoad(gameObject);
+        }
+
+        public void SetAimCursor()
+        {
+            UnityEngine.Cursor.SetCursor(aimCursor, new Vector2(16, 48), CursorMode.Auto);
+        }
+        
+        public void SetReloadCursor()
+        {
+            UnityEngine.Cursor.SetCursor(reloadCursor, new Vector2(16, 48), CursorMode.Auto);
         }
     }
 }
