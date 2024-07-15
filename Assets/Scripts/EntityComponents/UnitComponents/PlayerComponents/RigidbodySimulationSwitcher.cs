1de1c56229ff6f58c8c4e34d39cd54efe9326c7e
Ninellie <a.pawlow2009@ya.ru>
2024-07-15 05:38:15 +0300

    Added RigidbodySimulationSwitcher.cs

---
+++ Assets/Scripts/EntityComponents/UnitComponents/PlayerComponents/RigidbodySimulationSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Core.Sets;
+using UnityEngine;
+
+namespace EntityComponents.UnitComponents.PlayerComponents
+{
+    public class RigidbodySimulationSwitcher : MonoBehaviour
+    {
+        [SerializeField] private ShooterRuntimeSet runtimeSet;
+        [SerializeField] private bool simulationEnabled;
+        
+        private void OnEnable()
+        {
+            simulationEnabled = true;
+            UpdateSimulationState();
+        }
+
+        private void OnDisable()
+        {
+            simulationEnabled = false;
+            UpdateSimulationState();
+        }
+
+        private void OnValidate()
+        {
+            UpdateSimulationState();
+        }
+
+        public void UpdateSimulationState()
+        {
+            foreach (var item in runtimeSet.items)
+            {
+                if (item.TryGetComponent<Rigidbody2D>(out var rb2D))
+                {
+                    rb2D.simulated = simulationEnabled;
+                }
+            }
+        }
+    }
+}
